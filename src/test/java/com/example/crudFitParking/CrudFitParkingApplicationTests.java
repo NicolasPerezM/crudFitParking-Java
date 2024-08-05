65ffe9dffe87ad1c2d6980d77c7b25ef0c2c0fac
Nicolas Perez <nbome12@outlook.com>
2024-08-05 09:21:26 -0500

    initial commit

---
+++ src/test/java/com/example/crudFitParking/CrudFitParkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crudFitParking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudFitParkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
