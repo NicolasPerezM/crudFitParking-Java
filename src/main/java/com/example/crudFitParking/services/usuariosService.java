314b585d0acb51500f4c881a78adec4dae4b47d4
Nicolas Perez <nbome12@outlook.com>
2024-08-06 17:44:25 -0500

    Se terminó la creacion de los servicios

--- src/main/java/com/example/crudFitParking/services/usuariosService.java
+++ src/main/java/com/example/crudFitParking/services/usuariosService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import com.example.crudFitParking.repositories.IUUsuarios;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class usuariosService  {
@@ -16,7 +17,19 @@ public class usuariosService  {
         IUsuarios.save(usuarios);
     }
 
-    public List<com.example.crudFitParking.models.usuariosModel> findAll() {
+    //Read
+    public List<com.example.crudFitParking.models.usuariosModel> getUsuarios() {
         return IUsuarios.findAll();
     }
+
+    //Read by Id
+    public Optional<com.example.crudFitParking.models.usuariosModel> getUsuario(Long id) {
+       return IUsuarios.findById(id);
+    }
+
+    //Delete by id
+    public void deleteUsuario(Long id) {
+        IUsuarios.deleteById(id);
+    }
 }
+    
