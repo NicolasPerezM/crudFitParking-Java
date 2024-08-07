b59a989789b1d30b83150b394a108b916da8844a
Nicolas Perez <nbome12@outlook.com>
2024-08-07 09:29:02 -0500

    modifique el repositorio para tratar de solucionar el error del post

--- src/main/java/com/example/crudFitParking/repositories/IUUsuarios.java
+++ src/main/java/com/example/crudFitParking/repositories/IUUsuarios.java
@@ -2,7 +2,9 @@ package com.example.crudFitParking.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import com.example.crudFitParking.models.usuariosModel;
+
 @Repository
-public interface IUUsuarios extends JpaRepository<com.example.crudFitParking.models.usuariosModel, Long> {
+public interface IUUsuarios extends JpaRepository<usuariosModel, Long> {
 
 }
