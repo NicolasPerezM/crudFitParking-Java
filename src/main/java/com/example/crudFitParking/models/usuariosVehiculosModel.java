850240d4615db676d2c07df18dd6b222f8fd0cd7
Nicolas Perez <nbome12@outlook.com>
2024-09-02 08:57:29 -0500

    usuariosVehiculosModel con id autoincrement

--- src/main/java/com/example/crudFitParking/models/usuariosVehiculosModel.java
+++ src/main/java/com/example/crudFitParking/models/usuariosVehiculosModel.java
@@ -1,5 +1,43 @@
 package com.example.crudFitParking.models;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="usuarios_vehiculos")
 public class usuariosVehiculosModel {
 
-}
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    
+    @Column
+    private Long idUsuarios;
+
+    @Column
+    private Long idVehiculos;
+
+    public Long getIdUsuarios() {
+        return idUsuarios;
+    }
+
+    public Long getIdVehiculos() {
+        return idVehiculos;
+    }
+
+    public void setIdUsuarios(Long idUsuarios) {
+        this.idUsuarios = idUsuarios;
+    }
+
+    public void setIdVehiculos(Long idVehiculos) {
+        this.idVehiculos = idVehiculos;
+    } 
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}  
